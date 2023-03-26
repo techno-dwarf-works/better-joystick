55a08398ba7ca5d0f66d02c7b95b60ba65a76f03
uurha <uurha@users.noreply.github.com>
2023-03-26 00:15:41 +0000

    Merge pull request #5 from uurha/dev
    Dev

---
+++ Runtime/Scripts/Models/JoystickEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace BetterJoystick.Runtime.Models
+{
+    [Serializable]
+    public class JoystickEvent : ChangeEvent<Vector2>
+    {
+            
+    }
+}
